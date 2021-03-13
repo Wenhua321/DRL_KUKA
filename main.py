721beecb43010b103f8a178c35c91fd6372a0e17
2142431167wwh@sjtu.edu.cn <2142431167wwh@sjtu.edu.cn>
2021-03-13 21:31:51 +0800

    new

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 Double Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
+import torch
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
@@ -13,4 +9,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
